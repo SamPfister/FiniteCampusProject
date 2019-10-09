6dc7027acd32bae5d6d715db098ccb03af110a1e
SamPfister <Sam.Pfister.550@gmail.com>
2019-10-09 13:46:18 -0500

    end of day 10/8/2019

--- app/src/main/java/com/example/finitecampusproject/AddClassActivity.java
+++ app/src/main/java/com/example/finitecampusproject/AddClassActivity.java
@@ -2,7 +2,10 @@ package com.example.finitecampusproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddClassActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class AddClassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_class);
+        Button button = (Button) findViewById(R.id.submitButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddClassActivity.this, ClassListActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.cancelButton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddClassActivity.this, ClassListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/finitecampusproject/ClassActivity.java
+++ app/src/main/java/com/example/finitecampusproject/ClassActivity.java
@@ -2,7 +2,10 @@ package com.example.finitecampusproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ClassActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class ClassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class);
+        Button button = (Button) findViewById(R.id.submitButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClassActivity.this, ViewAllAssignmentsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
