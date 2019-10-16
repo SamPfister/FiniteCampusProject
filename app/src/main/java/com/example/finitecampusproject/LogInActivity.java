898f57ccb33c5704293fc591c22be46725a6f86d
SamPfister <Sam.Pfister.550@gmail.com>
2019-10-16 13:29:39 -0500

    I added custom buttons to the LogIn and SignUp activities

--- app/src/main/java/com/example/finitecampusproject/LogInActivity.java
+++ app/src/main/java/com/example/finitecampusproject/LogInActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -25,8 +26,33 @@ public class LogInActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LogInActivity.this, ClassListActivity.class);
-                startActivity(intent);
+                EditText nameEditText = (EditText)findViewById(R.id.editText);
+                EditText passwordEditText = (EditText)findViewById(R.id.editText2);
+                EditText confirmPasswordEditText = (EditText)findViewById(R.id.editText3);
+                String name = nameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+                String confirmPassword = confirmPasswordEditText.getText().toString().trim();
+                boolean check = true;
+                if(name.isEmpty()){
+                    nameEditText.setError("Name cannot be empty");
+                    check = false;
+                }
+                if(password.isEmpty()){
+                    passwordEditText.setError("Password cannot be empty");
+                    check = false;
+                }
+                if(confirmPassword.isEmpty()){
+                    confirmPasswordEditText.setError("Confirm Password cannot be empty");
+                    check = false;
+                }
+                if(!(confirmPassword.equals(password))){
+                    confirmPasswordEditText.setError("Passwords do not match");
+                    check = false;
+                }
+                if(check){
+                    Intent intent = new Intent(LogInActivity.this, ClassListActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
--- app/src/main/java/com/example/finitecampusproject/SignUpActivity.java
+++ app/src/main/java/com/example/finitecampusproject/SignUpActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -14,13 +15,6 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         Button button = (Button) findViewById(R.id.submitButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignUpActivity.this, LogInActivity.class);
-                startActivity(intent);
-            }
-        });
         Button button2 = (Button) findViewById(R.id.cancelButton);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +23,38 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nameEditText = (EditText)findViewById(R.id.name);
+                EditText passwordEditText = (EditText)findViewById(R.id.password);
+                EditText confirmPasswordEditText = (EditText)findViewById(R.id.confirmPassword);
+                String name = nameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+                String confirmPassword = confirmPasswordEditText.getText().toString().trim();
+                boolean check = true;
+                if(name.isEmpty()){
+                    nameEditText.setError("Name cannot be empty");
+                    check = false;
+                }
+                if(password.isEmpty()){
+                    passwordEditText.setError("Password cannot be empty");
+                    check = false;
+                }
+                if(confirmPassword.isEmpty()){
+                    confirmPasswordEditText.setError("Confirm Password cannot be empty");
+                    check = false;
+                }
+                if(!(confirmPassword.equals(password))){
+                    confirmPasswordEditText.setError("Passwords do not match");
+                    check = false;
+                }
+                if(check){
+                    Intent intent = new Intent(SignUpActivity.this, ClassListActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
+
 }
